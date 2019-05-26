b64b39d6cac4cf1d76dd7e0114d8072a5978c4f1
Cody Frank <cody.frank30@gmail.com>
2019-05-26 19:58:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,9 @@
 class Triangle
-  # write code here
+  attr_reader :side_one, :side_two, :side_three
+
+  def initialize (num1, num2, num3)
+    @side_one = num1
+    @side_two = num2
+    @side_three = num3
+  end
 end
