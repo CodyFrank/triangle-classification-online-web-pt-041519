a3594e4c54744694137b56bee1f8bb991c83fe6a
Cody Frank <cody.frank30@gmail.com>
2019-05-26 20:30:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,7 +16,7 @@ class Triangle
   end
 
   def isosceles?
-    if @side_one == @side_two || @side_three
+    if @side_one == @side_two || @side_three == @side_one
       true
     else
       false
