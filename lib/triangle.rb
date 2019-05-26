e09fc275d47da6ecfe9ced46dc3276e0d434f520
Cody Frank <cody.frank30@gmail.com>
2019-05-26 21:09:31 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,67 @@
 class Triangle
-  # write code here
+  attr_accessor :side_one, :side_two, :side_three
+
+  def initialize (num1, num2, num3)
+    @side_one = num1.to_f
+    @side_two = num2.to_f
+    @side_three = num3.to_f
+  end
+
+  def equilateral?
+    if @side_one == @side_two && @side_three == @side_one
+      true
+    else
+      false
+    end
+  end
+
+  def isosceles?
+    if @side_one == @side_two || @side_three == @side_one || @side_two == @side_three
+      true
+    else
+      false
+    end
+  end
+
+  def scalene?
+    if @side_one != @side_two && @side_one != @side_three && @side_two != @side_three
+      true
+    else
+      false
+    end
+  end
+
+  def valid?
+    valid = true
+    if @side_one + @side_two <= @side_three
+      valid = false
+    elsif @side_three + @side_one <= @side_two
+      valid = false
+    elsif @side_three + @side_two <= @side_one
+      valid = false
+    elsif @side_one <= 0 && @side_two <= 0
+      valid = false
+    elsif @side_three <= 0
+      valid = false
+    end
+    return valid
+  end
+
+  def kind
+    if valid? == false
+      raise TriangleError
+    elsif equilateral?
+      :equilateral
+    elsif isosceles?
+      :isosceles
+    elsif scalene?
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This is a Triangle Error message"
+    end
+  end
 end
