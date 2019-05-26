82afa3d39a061e99560dfbc678f3d26fdd9274e4
Cody Frank <cody.frank30@gmail.com>
2019-05-26 20:59:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -34,8 +34,8 @@ class Triangle
   def valid?
     valid = true
     if @side_one + @side_two <= @side_three && @side_three + @side_one <= @side_two
-      if @side_one <= 0 && @side_two <= 0
-        if @side_three <= 0
+      if @side_one >= 0 && @side_two >= 0
+        if @side_three >= 0
           valid = false
         end
       end
