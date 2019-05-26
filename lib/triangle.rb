322195cb8d7dc1dbd3312f6e01c9bba5342fb66f
Cody Frank <cody.frank30@gmail.com>
2019-05-26 20:29:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,9 +2,9 @@ class Triangle
   attr_reader :side_one, :side_two, :side_three
 
   def initialize (num1, num2, num3)
-    @side_one = num1
-    @side_two = num2
-    @side_three = num3
+    @side_one = num1.to_f
+    @side_two = num2.to_f
+    @side_three = num3.to_f
   end
 
   def equilateral?
