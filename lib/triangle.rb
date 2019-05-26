12c7372152598aefeed304b3a5105d8b7b770a0f
Cody Frank <cody.frank30@gmail.com>
2019-05-26 20:25:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -24,7 +24,7 @@ class Triangle
   end
 
   def scalene?
-    if @side_one != @side_two || @side_three
+    if @side_one != @side_two && @side_three
       if @side_two != @side_three
         true
       end
